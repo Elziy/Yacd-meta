4324b1736de0378c50abd0ab72d57a5ea7a4e000
Elziy <elziy6@163.com>
2024-05-31 21:34:23 +0800

    chore: Merge direct rule statistic

--- src/components/home/charts/RuleStatisticChart.tsx
+++ src/components/home/charts/RuleStatisticChart.tsx
@@ -8,6 +8,20 @@ import { connect } from '~/components/StateProvider';
 function getRuleStatistic(ruleStatistic: RuleStatistic[], minTraffic: number) {
   ruleStatistic = ruleStatistic.filter((item) => item.upload + item.download > minTraffic * 1024 * 1024).length >= 1 ?
     ruleStatistic.filter((item) => item.upload + item.download > minTraffic * 1024 * 1024) : ruleStatistic;
+  // 找出所有含有CN的规则
+  const cnRules = ruleStatistic.filter((item) => item.rule.toLowerCase().includes('cn'));
+  // 找出所有含有Direct的规则
+  const directRules = ruleStatistic.filter((item) => item.rule.toLowerCase().includes('direct'));
+  // 合并CN和Direct规则为Direct
+  const directRule = {
+    rule: 'Direct',
+    upload: cnRules.reduce((acc, item) => acc + item.upload, 0) + directRules.reduce((acc, item) => acc + item.upload, 0),
+    download: cnRules.reduce((acc, item) => acc + item.download, 0) + directRules.reduce((acc, item) => acc + item.download, 0)
+  };
+  // 删除CN和Direct规则
+  ruleStatistic = ruleStatistic.filter((item) => !item.rule.toLowerCase().includes('cn') && !item.rule.toLowerCase().includes('direct'));
+  // 添加合并后的Direct规则
+  ruleStatistic.push(directRule);
   return ruleStatistic
     .sort((a, b) => b.upload + b.download - a.upload - a.download);
 }
